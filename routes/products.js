154c19f7eb467320d4ac24162cff945fcdf4d341
zapiralos <zapiralos@gmail.com>
2020-06-09 09:09:50 -0300

    maquetado profile terminado

--- routes/products.js
+++ routes/products.js
@@ -7,16 +7,17 @@ const guestMiddleware = require("../middlewares/guestMiddleware");
 const userMiddleware = require("../middlewares/userMiddlware");
 
 var storage = multer.diskStorage({
-  destination: path.join(__dirname, "../public/upload/"),
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+    destination: path.join(__dirname, "../public/upload/"),
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
+    },
 });
 
 var upload = multer({ storage: storage });
+
 // ************ Controller Require ************
 const controller = require("../controllers/productsController");
 const controllerProducts = require("../controllers/newproducts");
@@ -28,20 +29,20 @@ router.get("/", controller.allproducts);
 router.get("/:id?", controller.detailproduct);
 
 router.get(
-  "/:id/edit",
-  userMiddleware,
-  controller.edit
+    "/:id/edit",
+    userMiddleware,
+    controller.edit
 ); /* GET - Product edit */
 router.put(
-  "/:id?",
-  userMiddleware,
-  upload.single("image1"),
-  controller.update
+    "/:id?",
+    userMiddleware,
+    upload.single("image1"),
+    controller.update
 ); /* PUT - Product update */
 router.get("/pruebaSession", function (req, res) {
-  if (req.session.numero == undefined) {
-    req.session.numero = 0;
-  }
+    if (req.session.numero == undefined) {
+        req.session.numero = 0;
+    }
 });
 
 /*Crear Productos*/
@@ -52,9 +53,9 @@ router.post("/vender", upload.single("image1"), controllerProducts.register);
 /*** DELETE ONE PRODUCT***/
 // /produdcts/delete/:id
 router.delete(
-  "/:id/delete",
-  userMiddleware,
-  controller.destroy
+    "/:id/delete",
+    userMiddleware,
+    controller.destroy
 ); /* DELETE - Delete from DB */
 
 module.exports = router;

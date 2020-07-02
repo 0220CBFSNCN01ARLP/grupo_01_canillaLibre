2207b948f5730606c485fe20ea5717506ed0ca76
zapiralos <zapiralos@gmail.com>
2020-07-02 12:58:45 -0300

    poblacion

---
+++ poblacion/ScriptPoblacion-productos.sql
@@ -0,0 +1,47 @@
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593663424730.png", NULL, 0, 0, NULL, NULL);
+
+INSERT INTO canillalibre.producto
+(usuarioId, nombre, precioUnitario, descuento, descripcion, imagen, rating, stock, tipoproducto, create_at, update_at)
+VALUES(NULL, 'Corona Extra', 100, 10, "Corona Extra Mexican Beer is an even-keeled import beer with fruity-honey aromas and a touch of malt.", "imagen-1593661553156.png", NULL, 0, 0, NULL, NULL);

a933f5f7efcd1d2e06b7103145dee3ac3f3499f0
francomartin20 <fmfrancomartin@gmail.com>
2020-07-12 16:04:57 -0300

    Validador modifar productos

---
+++ public/js/tipoProducto.js
@@ -0,0 +1,14 @@
+function tipoProducto() {
+
+    var productoId = document.getElementById("productoId");
+
+    var disertante = document.getElementById("disertante");
+    var medioId = document.getElementById("medioId");
+
+
+    if(productoId.value == "1")
+    disertante.style.display = "none";
+  
+  }
+  
+  tipoProducto();

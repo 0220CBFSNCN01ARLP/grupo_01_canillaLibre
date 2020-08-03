4199994ef3cacdac66cea78518fa703bef775e67
francomartin20 <fmfrancomartin@gmail.com>
2020-08-03 13:49:49 -0300

    Agrego archivo storage.js

--- controllers/carritoController.js
+++ controllers/carritoController.js
@@ -1,59 +1,63 @@
 const fs = require("fs");
 const path = require("path");
-
 const {
-    Productos,
-    Bebidas,
-    Cursos,
-    Insumos,
-    Presentacion,
-    Medio,
-    Usuarios,
+  Productos,
+  Bebidas,
+  Cursos,
+  Insumos,
+  Presentacion,
+  Medio,
+  Usuarios,
+  Sequelize,
 } = require("../database/models");
-
+const Op = Sequelize.Op;
 const carritoController = {
-    addshopcart: async (req, res) => {
-        // try {
-        const usuarioLogueado = req.session.usuarioLogueado;
-        const product = await Productos.findByPk(req.params.id, {
-        include: [{ association: "usuario" }],
-        });
-
-        console.log("los pprdocutos que levantan son " + product);
-        if(!req.session.carrito){
-            req.session.carrito=[];
-        }
-        if (usuarioLogueado.email != product.usuario.email) {
-        
-            req.session.carrito.push({
-                id: product.id,
-                cantidad: 1
-            });
-        
-        res.redirect("/carrito");
-        // res.send(carrito) // modal que avise que se cargo producto xx al carrito
-        } else {
-        res.send("El usuario logueado no puede comprar sus propios productos");
-        }
-    },
-    allshopcart: (req, res) => {
-        if(!req.session.carrito){
-            req.session.carrito=[];
-        }
-        res.render("shop-cart", { carrito: req.session.carrito });
-    },
-  
-    deletefromshopcart: async (req, res) => {
-        let i = req.session.carrito.findIndex( (e)=>{
-            return e.id == req.params.id;
-        });
-            if (i !== -1) {
-                req.session.carrito.splice(i, 1);
-            }
-        res.redirect("/carrito");
-        res.send(req.session.carrito)
-    },
-  
+  addshopcart: async (req, res) => {
+    // try {
+    const usuarioLogueado = req.session.usuarioLogueado;
+    const product = await Productos.findByPk(req.params.id, {
+      include: [{ association: "usuario" }],
+    });
+    console.log("los pprdocutos que levantan son " + product);
+    if (!req.session.carrito) {
+      req.session.carrito = [];
+    }
+    if (usuarioLogueado.email != product.usuario.email) {
+      //req.session.carrito.push(product)
+      req.session.carrito.push({
+        id: product.id,
+        cantidad: 1,
+      });
+      //res.render("shop-cart", { carrito: req.session.carrito });
+      res.redirect("/carrito");
+      // res.send(carrito) // modal que avise que se cargo producto xx al carrito
+    } else {
+      res.send("El usuario logueado no puede comprar sus propios productos");
+    }
+  },
+  allshopcart: async (req, res) => {
+    if (!req.session.carrito) {
+      req.session.carrito = [];
+    }
+    let items = req.session.carrito.map((e) => {
+      return e.id;
+    });
+    const carrito = await Productos.findAll({
+      where: {
+        id: { [Op.in]: items },
+      },
+    });
+    res.render("shop-cart", { carrito });
+  },
+  deletefromshopcart: async (req, res) => {
+    let i = req.session.carrito.findIndex((e) => {
+      return e.id == req.params.id;
+    });
+    if (i !== -1) {
+      req.session.carrito.splice(i, 1);
+    }
+    res.redirect("/carrito");
+    res.send(req.session.carrito);
+  },
 };
-
 module.exports = carritoController;

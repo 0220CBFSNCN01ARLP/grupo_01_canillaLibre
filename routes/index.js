d14d4735c94d874ca93cb497ae2e45df240c68b9
diegodh22 <diegoratti45@hotmail.com>
2020-08-07 10:06:14 -0300

    agregado de botones de carrito al detalle del producto segun si el usuario es vendedor o no

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ const { Productos } = require("../database/models");
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
-    const products = await Productos.findAll();
+    const products = await Productos.findAll({ limit: 10 });
     console.log("el producto del index es: " + products);
     res.render("index", { products });
 });
 
 /* GET team */
-router.get("/nosotros", function (req, res){
+router.get("/nosotros", function (req, res) {
     res.render("team");
-})
+});
 
 /* Product_cart*/
 router.get("/product_cart.ejs", function (req, res) {
